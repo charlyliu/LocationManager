b027059be7582791692ad8d730d75df37790ea53
Charly Liu Chou <charly.liu@schibsted.es>
2015-05-22 16:47:44 +0200

    Add Framework

---
+++ LocationManagerExample/LocationManagerFramework/LocationManagerFramework.h
@@ -0,0 +1,23 @@
+//
+//  LocationManagerFramework.h
+//  LocationManagerFramework
+//
+//  Created by Charly Liu Chou on 5/22/15.
+//  Copyright (c) 2015 Intuit Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocationManagerFramework.
+FOUNDATION_EXPORT double LocationManagerFrameworkVersionNumber;
+
+//! Project version string for LocationManagerFramework.
+FOUNDATION_EXPORT const unsigned char LocationManagerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationManagerFramework/PublicHeader.h>
+
+
+#import "INTULocationManager.h"
+#import "INTULocationRequest.h"
+#import "INTULocationRequestDefines.h"
+#import "INTULocationManager+Internal.h"
---
+++ LocationManagerExample/LocationManagerFrameworkTests/LocationManagerFrameworkTests.m
@@ -0,0 +1,40 @@
+//
+//  LocationManagerFrameworkTests.m
+//  LocationManagerFrameworkTests
+//
+//  Created by Charly Liu Chou on 5/22/15.
+//  Copyright (c) 2015 Intuit Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LocationManagerFrameworkTests : XCTestCase
+
+@end
+
+@implementation LocationManagerFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
